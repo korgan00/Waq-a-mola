a5844b8bf6f8609bddc7522605adcdfbd2ad236c
Korgan <korgan00@gmail.com>
2019-06-21 17:22:52 +0200

    "Quantic field" random animation and random spawn

---
+++ Assets/Project/Scripts/PerVertexMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerVertexMove : MonoBehaviour {
+
+    private Mesh _mesh;
+    private Vector3[] _verticesSrc;
+    private Vector3[] _verticesTarget;
+    private Vector3[] _verticesLastPosition;
+    private Vector3[] _currentVertices;
+
+    private QASMRandomProvider _randomProvider;
+
+    private float _currentTransition = 0;
+
+    void Start() {
+        _mesh = GetComponent<MeshFilter>().mesh;
+
+        _verticesSrc = _mesh.vertices;
+        _currentVertices = _mesh.vertices;
+        _verticesTarget = new Vector3[_currentVertices.Length];
+        _verticesLastPosition = new Vector3[_currentVertices.Length];
+
+        _randomProvider = FindObjectOfType<QASMRandomProvider>();
+
+        StartCoroutine(ChangeVerticesPosition());
+    }
+
+    public IEnumerator ChangeVerticesPosition() {
+        bool requested = false;
+        List<float> offsets = null;
+        _randomProvider.GenerateFloatPool(_verticesTarget.Length * 6, (pool) => {
+            offsets = pool;
+        });
+
+        while (true) {
+            yield return new WaitWhile(() => offsets == null || offsets.Count < _verticesTarget.Length);
+
+            _currentTransition = 0;
+            for (int i = 0; i < _verticesTarget.Length; i++) {
+                _verticesLastPosition[i] = _currentVertices[i];
+                _verticesTarget[i] = _verticesSrc[i] + Vector3.up * offsets[0];
+                offsets.RemoveAt(0);
+            }
+
+            if (!requested && offsets.Count < _verticesTarget.Length * 3) {
+                requested = true;
+                _randomProvider.GenerateFloatPool(_verticesTarget.Length * 6, (pool) => {
+                    offsets.AddRange(pool);
+                    requested = false;
+                });
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void Update() {
+        _currentTransition += Time.deltaTime;
+        for (int i = 0; i < _currentVertices.Length; i++) {
+            _currentVertices[i] = Vector3.Lerp(_verticesLastPosition[i], _verticesTarget[i], _currentTransition);
+        }
+        
+        _mesh.vertices = _currentVertices;
+        _mesh.RecalculateBounds();
+    }
+}
---
+++ Assets/Project/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnedObject;
+
+    List<float> _spawnRandoms = null;
+
+    public QASMRandomProvider _randomProvider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomProvider = FindObjectOfType<QASMRandomProvider>();
+        _randomProvider.GenerateFloatPool(100, (pool) => {
+            _spawnRandoms = pool;
+            StartCoroutine(SpawnPeriodically());
+        });
+    }
+
+
+    public IEnumerator SpawnPeriodically() {
+        while (_spawnRandoms.Count >= 2) {
+            yield return new WaitForSeconds(_spawnRandoms[0] * 4f + 1);
+            _spawnRandoms.RemoveAt(0);
+            GameObject go = Instantiate(spawnedObject);
+            go.transform.localPosition = SamplePositionXZ(-1f, Vector2.one * -4, Vector2.one * 4);
+            
+        }
+    }
+
+    public Vector3 SamplePositionXZ(float height, Vector2 min, Vector2 max) {
+        Vector2 diff = max - min;
+
+        float x = _spawnRandoms[0] * diff.x + min.x;
+        float y = _spawnRandoms[1] * diff.y + min.y;
+
+        _spawnRandoms.RemoveRange(0, 2);
+
+        return new Vector3(x, height, y);
+    }
+}
